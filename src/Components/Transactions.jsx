b8d81c444bd63fe60f4b86beb6b0adc93b450d0a
abhi-13-07 <abhishek.13.july.02@gmail.com>
2021-01-03 01:40:15 +0530

    add delete functionality

--- src/Components/Transactions.jsx
+++ src/Components/Transactions.jsx
@@ -2,11 +2,22 @@ import React, { useContext } from 'react';
 import { TransactionContext } from '../Context/TransactionProvider';
 
 const Transactions = () => {
-	const [transactions] = useContext(TransactionContext);
+	const [transactions, setTransactions] = useContext(TransactionContext);
+
+	const deleteTransaction = (id) => {
+		setTransactions(
+			transactions.filter((transaction) => transaction.id !== id)
+		);
+	};
+
 	return (
 		<div className="transactions-container">
 			{transactions.map((transaction) => (
-				<div className={`transaction ${transaction.type}`} key={transaction.id}>
+				<div
+					className={`transaction ${transaction.type}`}
+					key={transaction.id}
+					onDoubleClick={() => deleteTransaction(transaction.id)}
+					style={{ cursor: 'pointer' }}>
 					<span>{transaction.comment}</span>
 					<span>{transaction.ammount}</span>
 				</div>
