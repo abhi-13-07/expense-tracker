cd74e214d400fe13861edcb0172c471f1651b079
abhi-13-07 <abhishek.13.july.02@gmail.com>
2021-01-03 01:25:10 +0530

    add functionality

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TransactionContext } from '../Context/TransactionProvider';
 
 const Header = () => {
+	const [transactions] = useContext(TransactionContext);
+
+	function addAmmount(arr, str) {
+		let result = 0;
+		arr.forEach((elem) => {
+			if (elem.ammount[0] === str) {
+				const ammount = parseFloat(elem.ammount.split(str)[1]);
+				result += ammount;
+			}
+		});
+		return result;
+	}
+
+	const incomes = transactions.filter(
+		(transaction) => transaction.ammount[0] !== '-'
+	);
+	const expenses = transactions.filter(
+		(transaction) => transaction.ammount[0] !== '+'
+	);
+
+	const totalIncome = addAmmount(incomes, '+');
+	const totalExpense = addAmmount(expenses, '-');
+
+	const balance = totalIncome - totalExpense;
+
 	return (
 		<header>
 			<div className="header-section">
 				<span style={{ fontSize: '1.5rem' }}>Expense Tracker</span>
 				<div style={{ textAlign: 'center', marginTop: '1.5rem' }}>
 					<h3>Your Balance</h3>
-					<h1>₹6000</h1>
+					<h1>₹{balance}</h1>
 				</div>
 				<div className="expense-container">
 					<div>
 						<span>Your Income</span>
-						<br />
-						₹5000
+						<br />₹{totalIncome}
 					</div>
 					<div>
 						<span>Your Expense</span>
-						<br />
-						₹3000
+						<br />₹{totalExpense}
 					</div>
 				</div>
 			</div>
